2491d41be0c245574ae2c126a78950d19001af3f
Taisia Galkina <galkinattasya@gmail.com>
2023-10-04 23:15:45 +0300

    Initial Commit

---
+++ togalkinaPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  togalkinaPW2
+//
+//  Created by Тася Галкина on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

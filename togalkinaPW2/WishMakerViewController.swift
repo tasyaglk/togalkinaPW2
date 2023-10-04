bee8d2f6d80a815fb97abe0d583c315932890868
Taisia Galkina <galkinattasya@gmail.com>
2023-10-05 01:52:51 +0300

    добавлено два дополнительных способа обновить цвет

---
+++ togalkinaPW2/WishMakerViewController.swift
@@ -0,0 +1,153 @@
+//
+//  WishMakerViewController.swift
+//  togalkinaPW2
+//
+//  Created by Тася Галкина on 04.10.2023.
+//
+
+import Foundation
+import UIKit
+
+final class WishMakerViewController: UIViewController {
+    
+    enum Constants {
+        static let sliderMin: Double = 0
+        static let sliderMax: Double = 1
+        static let red: String = "Red"
+        static let green: String = "Green"
+        static let blue: String = "Blue"
+        static let stackRadius: CGFloat = 20
+        static let stackBottom: CGFloat = -40
+        static let stackLeading: CGFloat = 20
+    }
+    
+    let title1 = UILabel()
+    private var slidersHidden = false
+    private let toggleButton = UIButton(type: .system)
+    let descriptionn = UILabel()
+    let stack = UIStackView()
+    let colorCodeTextField = UITextField()
+    let applyColorButton = UIButton(type: .system)
+    
+    let sliderRed = CustomSlider(title: Constants.red, min: Constants.sliderMin, max: Constants.sliderMax)
+    let sliderGreen = CustomSlider(title: Constants.green, min: Constants.sliderMin, max: Constants.sliderMax)
+    let sliderBlue = CustomSlider(title: Constants.blue, min: Constants.sliderMin, max: Constants.sliderMax)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .slayPink
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .slayPink
+        
+        configureTitle()
+        configureDescription()
+        configureSliders()
+        configureToggleButton()
+        randomChange() //??
+    }
+    
+    private func configureTitle() {
+        title1.translatesAutoresizingMaskIntoConstraints = false
+        title1.text = "WishMaker"
+        title1.font = UIFont.boldSystemFont(ofSize: 32)
+        title1.textColor = .textBlue
+        
+        view.addSubview(title1)
+        NSLayoutConstraint.activate([
+            title1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            title1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30)
+        ])
+    }
+    
+    private func configureDescription() {
+        descriptionn.translatesAutoresizingMaskIntoConstraints = false
+        descriptionn.text = "This app will bring you joy and will ful ill three of your wishes!\n The first wish is to change the background color" //!!!!
+        descriptionn.font = UIFont.systemFont(ofSize: 16)
+        descriptionn.textColor = .textBlue
+        
+        view.addSubview(descriptionn)
+        NSLayoutConstraint.activate([
+            descriptionn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionn.topAnchor.constraint(equalTo: title1.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    private func configureSliders() {
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        view.addSubview(stack)
+        stack.layer.cornerRadius = Constants.stackRadius
+        stack.clipsToBounds = true
+        stack.backgroundColor = .slayPink
+        
+        for slider in [sliderRed, sliderGreen, sliderBlue] {
+            stack.addArrangedSubview(slider)
+        }
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.stackLeading),
+            stack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Constants.stackBottom)
+        ])
+        
+        for sliderColor in [sliderRed, sliderBlue, sliderGreen] {
+            sliderColor.valueChanged = { [weak self] value in
+                self?.view.backgroundColor = UIColor(red: CGFloat((self?.sliderRed.slider.value)!), green: CGFloat((self?.sliderGreen.slider.value)!), blue: CGFloat((self?.sliderBlue.slider.value)!), alpha: 1)
+            }
+        }
+        
+    }
+    
+    @objc private func toggleSliders() {
+        slidersHidden.toggle()
+        
+        if slidersHidden == false {
+            toggleButton.setTitle("Sliders are here", for: .normal)
+        } else {
+            toggleButton.setTitle("Sliders are hide", for: .normal)
+        }
+        
+        sliderRed.isHidden = slidersHidden
+        sliderGreen.isHidden = slidersHidden
+        sliderBlue.isHidden = slidersHidden
+    }
+    
+    
+    private func configureToggleButton() {
+        toggleButton.translatesAutoresizingMaskIntoConstraints = false
+        toggleButton.setTitle("Touch me", for: .normal)
+        toggleButton.setTitleColor(.textBlue, for: UIControl.State.normal)
+        toggleButton.addTarget(self, action: #selector(toggleSliders), for: .touchUpInside)
+        
+        view.addSubview(toggleButton)
+        NSLayoutConstraint.activate([
+            toggleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            toggleButton.topAnchor.constraint(equalTo: descriptionn.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    @objc private func randomizer(){
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    private func randomChange() {
+        let changeButton = UIButton()
+        changeButton.translatesAutoresizingMaskIntoConstraints = false
+        changeButton.setTitle("Random colour change", for: .normal)
+        changeButton.setTitleColor(.textBlue, for: UIControl.State.normal)
+        changeButton.addTarget(self, action: #selector(randomizer), for: .touchUpInside)
+        
+        view.addSubview(changeButton)
+        NSLayoutConstraint.activate([
+            changeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            changeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            ])
+    }
+    
+    
+}
